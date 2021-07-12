702588f5e44335a735bbc745e969874ec752d9b7
gitikab <gitika23689@gmail.com>
2021-07-12 18:47:15 +0530

    Floats in python

---
+++ floats.py
@@ -0,0 +1,21 @@
+add = 0.1 + 0.4
+print(add)
+
+subtract = 12.7 - 2.2
+print(subtract)
+
+multiply = 10.5 * 0.1
+print(multiply)
+
+divide = 12.5 / 0.5
+print(divide)
+
+exponent = 64.0 ** 0.5
+print(exponent)
+
+# Concatenating floats with strings
+amount = 225.50
+# Returns TypeError
+# message = "Your bill amount is Rs.  " + amount
+message = "Your bill amount is Rs.  " + str(amount)
+print(message)
