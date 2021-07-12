bcf7ff71f2e842991b46bd36f1c391e93ece71ac
gitikab <gitika23689@gmail.com>
2021-07-12 20:28:45 +0530

    List sorting, reversing and finding length

---
+++ lists_operations.py
@@ -0,0 +1,38 @@
+# Sorting a list permanently
+flowers = ["jasmine", "rose", "marigold", "tulips", "lily"]
+print(flowers)
+flowers.sort()
+print(flowers)
+flowers.sort(reverse=True)
+print(flowers)
+
+# Sorting a list without affecting the original list
+flowers = ["jasmine", "rose", "marigold", "tulips", "lily"]
+print(flowers)
+print(sorted(flowers))
+print(flowers)
+print(sorted(flowers, reverse=True))
+print(flowers)
+
+# Reversing a list permanently
+print(flowers)
+flowers.reverse()
+print(flowers)
+
+# Length of a list
+print(len(flowers))
+
+# IndexError when accessing index that doesn't exist
+# flowers[5]
+
+# Use last index instead
+print(flowers[-1])
+
+# Accessing last element can also return IndexError if list is empty
+empty_list = []
+empty_list[-1]
+
+
+
+
+
