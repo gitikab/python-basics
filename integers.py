c7229b59f4cb0ce943544c647d42782371e77a66
gitikab <gitika23689@gmail.com>
2021-07-12 18:46:59 +0530

    Embedding integers in strings

--- integers.py
+++ integers.py
@@ -20,3 +20,10 @@ expression_with_parenthesis = ((2 + 5) * 3 - 5) / 5
 print(expression_with_parenthesis)
 
 # Note that expression is promoted to a float if any of the operations return a float
+
+# Concatenating integers with strings
+age = 2
+# Returns TypeError
+# greeting = "Happy " + age + "nd Birthday!!"
+greeting = "Happy " + str(age) + "nd Birthday!!"
+print(greeting)
