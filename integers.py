c4b15821cff2ece34b68e5b2bc4873db7de6ed6c
gitikab <gitika23689@gmail.com>
2021-07-12 18:33:51 +0530

    Integers in Python

---
+++ integers.py
@@ -0,0 +1,22 @@
+add = 2 + 3
+print(add)
+
+subtract = 100 - 35
+print(subtract)
+
+multiply = 12 * 5
+print(multiply)
+
+divide = 120 / 6
+print(divide)
+
+exponent = 4 ** 3
+print(exponent)
+
+expression = 2 + 5 * 3 - 5 / 5
+print(expression)
+
+expression_with_parenthesis = ((2 + 5) * 3 - 5) / 5
+print(expression_with_parenthesis)
+
+# Note that expression is promoted to a float if any of the operations return a float
