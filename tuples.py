0b3ddc3f3e42df8125035d17241e8e5d4f264143
gitikab <gitika23689@gmail.com>
2021-07-15 19:22:48 +0530

    Tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# Tuple is an immutable list
+fruits = ("apple", "mango", "banana", "grapes")
+print(fruits[2])
+
+for fruit in fruits:
+    print(fruit)
+
+# TypeError if a tuple value is changed
+#fruits[0] = "Apple"
+
+# No error if a tuple variable is reassigned
+fruits = ("Pear", "Peach")
