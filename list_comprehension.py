8b310b19e8c161c7a16698259d71532ab295aad2
gitikab <gitika23689@gmail.com>
2021-07-15 18:30:27 +0530

    List Comprehensions

---
+++ list_comprehension.py
@@ -0,0 +1,11 @@
+# First 10 squares
+squares = [value**2 for value in range(1, 11)]
+print(squares)
+
+# Capitalize a list
+fruits = ["apple", "mango", "grapes", "orange"]
+print([fruit.title() for fruit in fruits])
+
+# Generate table of 3
+table_of_3 = [n * 3 for n in range(1, 11)]
+print(table_of_3)
