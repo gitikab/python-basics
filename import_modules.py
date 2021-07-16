22e54cd2298cd1e863bd58937b0280b5c06b50bd
gitikab <gitika23689@gmail.com>
2021-07-16 17:25:13 +0530

    Importing all functions from a module

--- import_modules.py
+++ import_modules.py
@@ -1,9 +1,11 @@
 import pizza_module
 from pasta_module import make_pasta, make_pesto_pasta
 from pasta_module import make_arabiatta_pasta as white_pasta
+from pizza_module import *
 
 
 pizza_module.make_pizza("Spinach", "Cheese")
 make_pasta("Penne", "Olives", "Cheese", sauce_type="pink")
 make_pesto_pasta("Spiralli", "Zucchini")
 white_pasta("Sphagetti", "sun dried tomato")
+hate_pizza()
--- pizza_module.py
+++ pizza_module.py
@@ -1,3 +1,7 @@
 def make_pizza(*toppings):
     for topping in toppings:
         print("Put " + topping + " on my pizza")
+
+
+def hate_pizza():
+    print("I hate pizza")
