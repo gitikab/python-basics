d03d55da61d7ef5c89a17a5da5f03f644acafbe1
gitikab <gitika23689@gmail.com>
2021-07-17 21:50:39 +0530

    Reading a file using relative file path Reading a file line by line

--- files/file_reader.py
+++ files/file_reader.py
@@ -1,3 +1,19 @@
 with open("story.txt") as story:
     contents = story.read()
     print(contents)
+
+with open("textfiles/another_story.txt") as another_story:
+    print(another_story.read())
+
+word_count = {}
+with open("story.txt") as story:
+    for line in story:
+        words = line.split()
+        for word in words:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+
+for word, times in word_count.items():
+    print(word + " occurs " + str(times) + " times")
