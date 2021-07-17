cd4edd6d7476262afdb3e1fb4b15a8a0bccbfb9f
gitikab <gitika23689@gmail.com>
2021-07-17 22:03:05 +0530

    Write to an empty file

---
+++ files/file_writer.py
@@ -0,0 +1,13 @@
+# Writing to an empty file - open file in write mode
+# File modes read-only r, write w, append a, read-write r+
+with open("aboutme.txt", 'w') as file:
+    file.write("I love Python\n")
+    file.write("I also love Java\n")
+
+lines = ["I love Python\n", "I love Java\n"]
+with open("textfiles/aboutme.txt", 'w') as file:
+    file.writelines(lines)
+
+
+
+
