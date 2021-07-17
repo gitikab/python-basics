3eaee68822b8bbc8a67a6a18963695fc5daadfa1
gitikab <gitika23689@gmail.com>
2021-07-17 22:46:22 +0530

    Refactor code to greet user

--- json/storing_data.py
+++ json/storing_data.py
@@ -1,14 +1,31 @@
 import json
 
-try:
-    with open("username.txt") as user:
-        username = json.load(user)
-except FileNotFoundError:
-    username = input("Please enter your name: ")
-    print("Hii " + username)
-    remember_me = input("Do you want me to remember you?: ")
-    if remember_me.lower() == 'yes':
-        with open("username.txt", 'w') as user:
-            json.dump(username, user)
-else:
-    print("Welcome back " + username)
+
+def get_stored_username():
+    try:
+        with open("username.txt") as stored_user:
+            return json.load(stored_user)
+    except FileNotFoundError:
+        return None
+
+
+def get_new_username():
+    return input("Please enter your name: ")
+
+
+def store_username(user_name):
+    with open("username.txt", 'w') as user:
+        json.dump(user_name, user)
+
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print("Welcome Back " + username)
+    else:
+        username = get_new_username()
+        store_username(username)
+        print("Hi " + username)
+
+
+greet_user()
